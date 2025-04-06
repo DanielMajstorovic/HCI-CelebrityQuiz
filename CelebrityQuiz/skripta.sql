746b6aba5e257dd07d60fb293f007fab7397b230
da-ni-e-l <116642398+DanielMajstorovic@users.noreply.github.com>
2025-04-06 19:57:24 +0200

    Update skripta.sql

--- CelebrityQuiz/skripta.sql
+++ CelebrityQuiz/skripta.sql
@@ -15,6 +15,12 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+-- -----------------------------------------------------
+-- Schema celebrityquiz
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `celebrityquiz` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `celebrityquiz` ;
+
 --
 -- Table structure for table `__efmigrationshistory`
 --
